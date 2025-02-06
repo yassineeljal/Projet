229180988b377a5339b49446fe9ae579bfdcd035
yassineeljal <yassineeljal0@gmail.com>
2025-02-05 23:37:55 -0500

    Hero corrigé

--- README.md
+++
@@ -1,5 +0,0 @@
-# Dépandance utilisé:
-- npm install tailwindcss @tailwindcss/vite
-- npm install @headlessui/react
-- npm install @heroicons/react
-- npm i -D react-router-dom@latest
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero/Hero.jsx";
+import Cards from "../components/Cards/Cards.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+
+function Home() {
+    return (
+        <>
+            <Hero/>
+            <Cards/>
+            <Footer/>
+        </>
+    );
+}
+
+export default Home;
