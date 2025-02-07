0ba3ae43f8d72252c85d70d70daf1f5f58ff1723
Yassine El Jal <2387923@collegemv.qc.ca>
2025-02-07 13:50:26 -0500

    api

--- frontend/src/components/Cards/CardsH.jsx
+++ frontend/src/components/Cards/CardsH.jsx
@@ -1,24 +1,30 @@
 import "./CardsH.css"
 function CardsH() {
+
+  API_URL="https://api.unsplash.com/search/photos"
+  useEffect(() => {
+      const { data } = await axios.get(`${API_URL}?query=${searchInput.current.value}&client_id=${import.meta.env.VITE_API_KEY}`)
+    });
+
+
     return (
         <>
-          
-<div className="flex justify-center items-center min-h-screen">
-  <div className="grid grid-cols-2 gap-x-16 gap-y-24 mt-30" style={{ paddingBottom: "70px" }}>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-  </div>
-</div>
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="grid grid-cols-2 gap-x-16 gap-y-24 mt-30" style={{ paddingBottom: "70px" }}>
+              <div className="cards w-240px h-80 flex items-center justify-center">
+                <figure className="card"></figure>
+              </div>
+              <div className="cards w-240px h-80 flex items-center justify-center">
+                <figure className="card"></figure>
+              </div>
+              <div className="cards w-240px h-80 flex items-center justify-center">
+                <figure className="card"></figure>
+              </div>
+              <div className="cards w-240px h-80 flex items-center justify-center">
+                <figure className="card"></figure>
+              </div>
+            </div>
+          </div>
         </>
     );
 }
