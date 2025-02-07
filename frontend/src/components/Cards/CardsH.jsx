5301e32c4c91ddcaf269603d57295e0544b8167a
Takfarinas <takfagaming@gmail.com>
2025-02-07 18:43:04 -0500

    avancement du projet pour l'API

--- frontend/src/components/Cards/CardsH.jsx
+++ frontend/src/components/Cards/CardsH.jsx
@@ -1,24 +1,31 @@
 import "./CardsH.css"
+import {useState,useEffect} from "react";
+import axios from "axios";
 function CardsH() {
-    return (
+
+
+    const API_URL="https://api.unsplash.com/search/photos"
+
+    const [image, setImage] = useState([])
+
+  useEffect(() => {
+    axios.get(`${API_URL}?query=${searchInput.current.value}&client_id=${import.meta.env.VITE_API_KEY}`)
+        .then(response => setImage(response.data))
+        .catch(err => console.log(err))
+  },[]);
+
+
+  return (
         <>
-          
-<div className="flex justify-center items-center min-h-screen">
-  <div className="grid grid-cols-2 gap-x-16 gap-y-24 mt-30" style={{ paddingBottom: "70px" }}>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-    <div className="cards w-240px h-80 flex items-center justify-center">
-      <figure className="card"></figure>
-    </div>
-  </div>
-</div>
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="grid grid-cols-2 gap-x-16 gap-y-24 mt-30" style={{ paddingBottom: "70px" }}>
+                {image.map((carte, i) => (
+                <div className="cards w-240px h-80 flex items-center justify-center" key={i}>
+                  <figure className="card" >{carte.link.self}</figure>
+                </div>
+              ))}
+            </div>
+          </div>
         </>
     );
 }
